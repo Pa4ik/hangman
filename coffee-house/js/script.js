0b823940331fbf844be6b447b467017b49b91164
Maxim Pachuzheuski <makspohp123@gmail.com>
2023-12-08 02:42:13 +0300

    feat:add overflow hiden to home page

--- coffee-house/js/script.js
+++ coffee-house/js/script.js
@@ -5,27 +5,31 @@ const navigation = nav?.querySelectorAll("a");
 // откртие закрытие на бургер
 burger?.addEventListener("click" , () => {
    burger?.classList.toggle("burger--active");
-   nav?.classList.toggle("navigation-visibl") ;
+   nav?.classList.toggle("navigation-visibl");
+   document.body.classList.toggle("hidden-overflow") 
 });
 
 navigation.forEach(el => {
     el.addEventListener("click", () => {
         burger?.classList.remove("burger--active");
-        nav?.classList.remove("navigation-visibl");   
+        nav?.classList.remove("navigation-visibl");
+        document.body.classList.remove("hidden-overflow")     
     })
 });
 //Закрытие на ecp и вне блока 
 window.addEventListener ("keydown", function(event){
     if (event.keyCode === 27){
         burger?.classList.remove("burger--active");
-        nav?.classList.remove("navigation-visibl");   
+        nav?.classList.remove("navigation-visibl"); 
+        document.body.classList.remove("hidden-overflow")    
     }
   })
 
 window.addEventListener("click", function(event){
     if (!event.target.classList.contains("burger--active") && !event.target.closest(".header-navigation")) {
       burger?.classList.remove("burger--active");
-      nav?.classList.remove("navigation-visibl");    
+      nav?.classList.remove("navigation-visibl");
+      document.body.classList.remove("hidden-overflow")      
     } 
   });
 
