59d053254a7af50a36d2465ebaf7c603662ba657
Maxim Pachuzheuski <makspohp123@gmail.com>
2023-12-08 02:28:25 +0300

    feat:switching of blocks in the menu has been done

--- coffee-house/js/script-menu.js
+++ coffee-house/js/script-menu.js
@@ -2,10 +2,12 @@
 const burger = document?.querySelector("[data-burger]");
 const nav = document?.querySelector("[data-nav]");
 const navigation = nav?.querySelectorAll("a");
+
 // откртие закрытие на бургер
 burger?.addEventListener("click" , () => {
    burger?.classList.toggle("burger--active");
    nav?.classList.toggle("navigation-visibl") ;
+   
 });
 
 navigation.forEach(el => {
@@ -28,3 +30,53 @@ window.addEventListener("click", function(event){
       nav?.classList.remove("navigation-visibl");    
     } 
   });
+
+
+
+  // Переключение блоков 
+  const btnMenuCoffee = document.querySelector(".menu-item-1");
+  const btnMenuTea = document.querySelector(".menu-item-2");
+  const btnMenuDesset = document.querySelector(".menu-item-3");
+  
+  const menuCoffee = document.querySelector(".menu-coffee");
+  const menuTea = document.querySelector(".menu-tea");
+  const menuDessert = document.querySelector(".menu-dessert");
+
+  
+  btnMenuCoffee?.addEventListener("click" , () => {
+    btnMenuCoffee?.classList.add("btn-menu-active");
+    btnMenuTea?.classList.remove("btn-menu-active");
+    btnMenuDesset?.classList.remove("btn-menu-active");
+    menuCoffee.classList.add("menu--active");
+    menuTea.classList.remove("menu--active");
+    menuDessert.classList.remove("menu--active");
+    menuCoffee.classList.remove("menu-hide")
+    menuTea.classList.add("menu-hide");
+    menuDessert.classList.add("menu-hide");
+ });
+
+ btnMenuTea?.addEventListener("click" , () => {
+  btnMenuTea?.classList.add("btn-menu-active");
+  btnMenuCoffee?.classList.remove("btn-menu-active");
+  btnMenuDesset?.classList.remove("btn-menu-active");
+  menuTea.classList.add("menu--active");
+  menuCoffee.classList.remove("menu--active");
+  menuDessert.classList.remove("menu--active");
+  menuCoffee.classList.add("menu-hide")
+  menuTea.classList.remove("menu-hide");
+  menuDessert.classList.add("menu-hide");
+});
+
+btnMenuDesset?.addEventListener("click" , () => {
+  btnMenuDesset?.classList.add("btn-menu-active");
+  btnMenuTea?.classList.remove("btn-menu-active");
+  btnMenuCoffee?.classList.remove("btn-menu-active");
+  menuDessert.classList.add("menu--active");
+  menuCoffee.classList.remove("menu--active");
+  menuTea.classList.remove("menu--active");
+  menuCoffee.classList.add("menu-hide")
+  menuTea.classList.add("menu-hide");
+  menuDessert.classList.remove("menu-hide");
+});
+
+ 
